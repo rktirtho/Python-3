9c6f136389c1a6c20313ba395f2d04837e62c844
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-29 22:45:59 +0600

    Python constructor and its parameters

--- class and inhertence/week 1/Point.py
+++ class and inhertence/week 1/Point.py
@@ -1,9 +1,10 @@
 class Point():
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
     def getPoint(self):
         return (self.x, self.y)
 
-point1 = Point()
-point2 = Point()
-point1.x=20
-point1.y=80
+point1 = Point(20,57)
+point2 = Point(56,28)
 print(point1.getPoint())
