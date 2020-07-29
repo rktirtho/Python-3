e0cd81c3b59643d10ae5d90a4c0baaf41e4bd748
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-29 21:11:23 +0600

    Nested Dictionary

--- Datacollection and Processing/nestedData.py
+++ Datacollection and Processing/nestedData.py
@@ -2,3 +2,21 @@ lst = [[4,6,3],[93,89,'fdf'],[743,35.6,36]]
 # print(lst)
 lst[1].append('Tirtho')
 print(lst)
+
+
+print("=================Dictionary======================")
+
+dic = {
+"user":{
+"name":"Rejaul Karin",
+"password": "792jf",
+"body":{
+"height":5.9,
+'eye' : 'Black'
+}
+}
+}
+
+print(dic)
+
+print(dic['user']['body']['eye'])
