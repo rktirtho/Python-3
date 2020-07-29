9d40c23a08f30dfcdabd60fb54ad0e7b6c36d42b
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-29 23:10:08 +0600

    add method in class

--- class and inhertence/week 1/Point.py
+++ class and inhertence/week 1/Point.py
@@ -6,7 +6,9 @@ class Point():
         return (self.x, self.y)
     def __str__(self):
         return "Point ({} {})".format(self.x,self.y)
+    def __add__(self,another):
+        return Point(self.x+another.x,self.y+another.y)
 
 point1 = Point(20,57)
 point2 = Point(56,28)
-print(point1)
+print(point1+point2)
