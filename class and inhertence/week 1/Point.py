b2687314fd645c835833ede09a9884c5755a7856
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-29 23:18:11 +0600

    Instances as return  values

--- class and inhertence/week 1/Point.py
+++ class and inhertence/week 1/Point.py
@@ -8,7 +8,15 @@ class Point():
         return "Point ({} {})".format(self.x,self.y)
     def __add__(self,another):
         return Point(self.x+another.x,self.y+another.y)
+    def __sub__(self,another):
+        return Point(self.x-another.x,self.y-another.y)
+    def halfWay(self,target):
+        mx=self.x+target.x /2
+        my = self.y+target.y / 2
+        return Point(mx,my)
 
-point1 = Point(20,57)
-point2 = Point(56,28)
-print(point1+point2)
+point1 = Point(10,30)
+point2 = Point(20,40)
+
+mid = point1.halfWay(point2)
+print(mid)
