94c55350018682f2d8f29d57cafcff6f93acc627
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-30 23:38:18 +0600

    inheritance file added

---
+++ class and inhertence/week-2/Inheritence.py
@@ -0,0 +1,10 @@
+class Person():
+    def __init__(self, name, birth_year):
+        this.name = name
+        this.birth_year = birth_year
+
+
+
+class Student(Person):
+    def __init(self, name, birth_year):
+        Person.__init__(self, name,birth_year)
