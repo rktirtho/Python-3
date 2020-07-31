07edfc614142d5c13dcdb462a9a35d42fdd50e5c
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-31 10:18:32 +0600

    Filter in python

--- Datacollection and Processing/week-2/mapAndfilter.py
+++ Datacollection and Processing/week-2/mapAndfilter.py
@@ -6,7 +6,20 @@ def tripleStaf(a_list):
     new_list = map(lambda value : 3*value, a_list)
     return new_list
 
-lst = [4,5,25]
+
+def evenList(a_list):
+    newList = list(filter(lambda value : value%2==0, a_list))
+    return newList
+
+def oddList(a_list):
+    new_list = filter(lambda value : value%2==1, a_list)
+    return new_list
+
+lst = [4,5,25,6,47,42,67,85,35,2,16,7]
 
 tpl = tripleStaf(lst)
+even = evenList(lst)
+odd = oddList(lst)
 print(list(tpl))
+print(list(odd))
+print(even)
