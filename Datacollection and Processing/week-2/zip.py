2105be8cef6f3fc48fc00a932757cfad314ca082
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-07-31 21:31:25 +0600

    zip in python

---
+++ Datacollection and Processing/week-2/zip.py
@@ -0,0 +1,9 @@
+names = ["Sam", 'John', "Pull", "Pitter", 'Sara', "Jin"]
+age = [23,42,12,23,56,34,]
+city = ['Machigan', 'Washington', 'New york', 'Virginia', 'Los Angles', 'Atlanta']
+
+
+persons = list(zip(names, age, city))
+
+n ,a,c = [m for m in persons]
+print(n)
