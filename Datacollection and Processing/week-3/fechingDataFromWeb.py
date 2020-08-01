823d5dd63e35614264b382a6c875693a4558626b
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-08-01 22:57:34 +0600

    Fetching a page

--- Datacollection and Processing/week-3/fechingDataFromWeb.py
+++ Datacollection and Processing/week-3/fechingDataFromWeb.py
@@ -4,4 +4,14 @@ import json
 
 page =  requests.get('https://api.datamuse.com/words/?rel_rhy=funny')
 
-print(page)
+print(type(page))
+
+response = page.json()
+
+print(type(response))
+print(response[0])
+
+print(type(response))
+
+res = json.dumps(response, indent=2)
+print(type(res))
